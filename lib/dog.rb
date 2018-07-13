aa0b9ad88b6eb106696daa2abed0abdbf1e22e7a
cassiebensko03 <cassidymbensko@gmail.com>
2018-07-13 17:00:58 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
 # dog.rb
+class Dog
+  
+  attr_accessor :name, :breed
+  def initialize(name, breed)
+    
+  @name=name
+  
+  @breed=dog
+  
+end
+end 
+
+dog_one = Dog.new("Kermit")
+
+puts dog_one.name
+puts dog_one.breed
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,15 @@
 # person.rb
+
+class Person
+  
+  attr_accessor :name
+  def initialize(name)
+    
+  @name=name
+  
+end
+end 
+
+person_one = Person.new("Jenna")
+
+puts person_one.name
